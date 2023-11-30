ad920726d2e4e6de92fb3a7b8ca2c0a0f46bdf8a
kw2343 <107032802+Kw2343@users.noreply.github.com>
2023-11-30 21:18:15 +1300

    Update Member.js

--- components/Member.js
+++ components/Member.js
@@ -7,17 +7,17 @@ const Member = () => {
   const [currentDateTime, setCurrentDateTime] = useState('');
 
   useEffect(() => {
-    // Get the current user
+ 
     const currentUser = auth.currentUser;
     if (currentUser) {
       setUser(currentUser);
       if (!currentUser.displayName) {
-        // If the user's display name is not set, fetch it from Firebase
+      
         fetchDisplayName(currentUser);
       }
     }
 
-    // Update the current date and time every second
+    
     const updateTime = () => {
       const now = new Date();
       setCurrentDateTime(now.toLocaleString());
@@ -30,7 +30,7 @@ const Member = () => {
     };
   }, []);
 
-  // Function to fetch the user's display name from Firebase
+  
   const fetchDisplayName = async (currentUser) => {
     try {
       const userDoc = await db.collection('users').doc(currentUser.uid).get();
@@ -74,37 +74,20 @@ const Member = () => {
         </div>
       </div>
       <div className="member-content">
-        {/* Add more content and features specific to your member portal here */}
+       
         <div className="portal-section">
           <h2>Your Membership Details</h2>
-          {/* Include fields for user-specific information, e.g., membership level, points, etc. */}
-          {/* Example:
-          <p>Membership Level: Premium</p>
-          <p>Points Earned: 500</p>
-          */}
+        
         </div>
 
         <div className="portal-section">
           <h2>Your Bookings</h2>
-          {/* Display a list of user's bookings with details like date, room type, etc. */}
-          {/* Example:
-          <ul>
-            <li>
-              <strong>Booking ID:</strong> 12345
-              <p><strong>Date:</strong> July 15, 2023</p>
-              <p><strong>Room Type:</strong> Single Room</p>
-            </li>
-            <!-- Add more booking items as needed -->
-          </ul>
-          */}
+      
         </div>
 
         <div className="portal-section">
           <h2>Additional Features</h2>
-          {/* Include any additional features or actions that users can perform */}
-          {/* Example:
-          <button className="custom-button">Upgrade Membership</button>
-          */}
+        
         </div>
       </div>
     </div>
